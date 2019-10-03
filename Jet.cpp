c42ac0df1dcc6e42c10936e61f6595360f85f786
Brendan Gressel <brendangressel8@gmail.com>
2019-10-03 10:10:01 -0400

    Finished Jet.cpp
    All functions have been implemented

--- Jet.cpp
+++ Jet.cpp
@@ -2,16 +2,29 @@
 #include "Jet.h"
 
 Jet::Jet(){
-    
+    setMileage(0);
+    setFuelType("unknown");
+    //number of engines is already set
 }
 
+//sets all values if they are passed in
 Jet::Jet(int e, double m, string fuelType){
-    
+    setEngines(e);
+    setMileage(m);
+    setFuelType(fuelType);
 }
 
-Jet::~Jet(){
-    
+Jet::~Jet()=default;
+
+//checks for the condition that the number of engines is above 2 and fuelType is Rocket
+void Jet::setEngines(int e){
+    numberofEngines=e;
+    if (e>2 && fuelType=="Rocket")
+        mileage = mileage + (mileage * 0.055);
 }
 
-void setEngines(int e); //sets engines
-void setMileage(int x); //sets mileage
+//sets milage and makes sure it follows conditions
+void Jet::setMileage(int x){
+    if  (x >= 40 && x<=100)
+        mileage=x;
+}
