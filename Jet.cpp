93b7b22209bff0ba6115ae38bd7cf2c4507d7ceb
Brendan Gressel <brendangressel8@gmail.com>
2019-10-03 10:01:17 -0400

    Finished Header

---
+++ Jet.cpp
@@ -0,0 +1,17 @@
+//Brendan Gressel
+#include "Jet.h"
+
+Jet::Jet(){
+    
+}
+
+Jet::Jet(int e, double m, string fuelType){
+    
+}
+
+Jet::~Jet(){
+    
+}
+
+void setEngines(int e); //sets engines
+void setMileage(int x); //sets mileage
---
+++ Jet.h
@@ -0,0 +1,25 @@
+//Brendan Gressel, Jet header
+
+#ifndef Jet_h
+#define Jet_h
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehicle {
+    
+private:
+    int numberofEngines=1;
+    double mileage;
+    string fuelType;
+    
+public:
+    Jet(); //defualts constucter
+    
+    explicit Jet(int e, double m, string fuelType); //sets all the private variables
+    
+    virtual ~Jet();
+    
+    void setEngines(int e); //sets engines
+    void setMileage(int x); //sets mileage
+};
+#endif /* Jet_h */
