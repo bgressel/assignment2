f61185a317f59841007a309629317a91d0e87e15
Brendan Gressel <brendangressel8@gmail.com>
2019-10-03 10:40:25 -0400

    Finished Scooter.cpp

--- Scooter.cpp
+++ Scooter.cpp
@@ -8,7 +8,17 @@ Scooter::Scooter(double m){
 
 Scooter::~Scooter()=default;
 
-//
+//mileage starts as a random number between 0.1 and 2
+//if time is over 15, mileage is doubled
+//if time is over 60, mileage=5
 double Scooter::mileageEstimate(double time){
-    
+    int temp;
+    mileage=rand() % 20 + 1;
+    mileage=mileage/10;
+    if  (time >= 15 ){
+        mileage= milage*2;
+    }
+    if  (time >= 60 ){
+        mileage= 5;
+    }
 }
